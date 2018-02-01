649b82c0ad89672ec02a754b2f89dbafc04e2adb
eafit-201710041010 <jjparrad@eafit.edu.co>
2018-02-01 18:52:58 -0500

    Progreso

--- talleres/taller01/Taller1/DigraphAL.java
+++ talleres/taller01/Taller1/DigraphAL.java
@@ -6,25 +6,32 @@ import java.util.ArrayList;
  * @author Mauricio Toro, Mateo Agudelo, <su nombre>
  */
 public class DigraphAL extends Digraph {
-	// complete...
+    public ArrayList<ArrayList<Pair<Integer, Integer>>> lista;
 
-	public DigraphAL(int size) {
-		super(size);
-		// complete...
-	}
+    public DigraphAL(int size) {
+        super(size);
+        lista = new ArrayList<>(size);
+        for(int i = 0; i < size; i++){
+            lista.set(i, new ArrayList<>());
+        }
+    }
 
-	public void addArc(int source, int destination, int weight) {
-		// complete...
-		// recuerde: grafo dirigido!
-	}
+    public void addArc(int source, int destination, int weight) {
+        ArrayList a = lista.get(source);
+        a.add(destination, weight);
+        // complete...
+        // recuerde: grafo dirigido!
+    }
 
-	public ArrayList<Integer> getSuccessors(int vertex) {
-		// complete...
-		// recuerde: null si no hay!
-	}
+    public ArrayList<Integer> getSuccessors(int vertex) {
+        // compl        ete...
+        // recuerde: null si no hay!
+        return null;
+    }
 
-	public int getWeight(int source, int destination) {
-		// complete...
-	}
+    public int getWeight(int source, int destination) {
+        // complete...
+        return 0;
+    }
 
 }
