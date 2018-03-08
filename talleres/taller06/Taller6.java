ca1089be03d667f4c5fafad07bd2e157f2f24fb1
Luisa Maria Vasquez Gomez <30305230+eafit-201710103010@users.noreply.github.com>
2018-03-08 11:19:37 -0500

    Add files via upload

--- talleres/taller06/Taller6.java
+++ talleres/taller06/Taller6.java
@@ -1,24 +1,31 @@
-
+import java.util.*;
 /**
- * Write a description of class s here.
+ * Clase en la cual se implementan los metodos del Taller de Clase #6
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Mauricio Toro, Mateo Agudelo
  */
-public class Taller6
-{
-    public static void main (String[] args){
-        int[] denominaciones = {500, 300, 200, 50};
-        cambioGreedy(1000,denominaciones);
-    }
-    public static int[] cambioGreedy(int n, int[] denominaciones) {
-        int [] devuelta = new int [denominaciones.length];
-        for(int i = 0; i < denominaciones.length; i++){
-            while(n >= denominaciones[i]){
-                n -= denominaciones[i];
-                devuelta[i]++;
-            }
-        }
-        return devuelta;
-    }
+public class Taller6 {
+
+	public static int[] cambioGreedy(int n, int[] denominaciones) {
+		return null;
+	}
+
+	public static int recorrido(Digraph g) {
+	    boolean[] visitados =new boolean[g.size()];
+		return recorrido(g,visitados,0,0);
+	}
+	
+	public static int recorrido(Digraph g, boolean[] visitados,int v,int suma) {
+	    visitados[v]=true;
+	    ArrayList<Integer> sucesores= g.getSuccessors(v);
+	    int minimo=g.size();
+	    for(int i =0;i<sucesores.size();i++){
+	        if(sucesores.get(i)<minimo && visitados[i]==false){
+	            minimo=i;
+	           }
+	    }
+		
+	    return suma+ recorrido(g,visitados,minimo,suma);
+	}
+
 }
