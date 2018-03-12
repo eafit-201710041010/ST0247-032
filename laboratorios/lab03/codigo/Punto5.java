aa0139729e76e244489bcc6662de52c0bb6409fe
Luisa Maria Vasquez Gomez <30305230+eafit-201710103010@users.noreply.github.com>
2018-03-11 20:45:13 -0500

    Documentando punto 5

--- laboratorios/lab03/codigo/Punto5.java
+++ laboratorios/lab03/codigo/Punto5.java
@@ -1,12 +1,17 @@
 import java.util.ArrayList;
 /**
- * Write a description of class Punto5 here.
+ * Solucion al punto 5 del Lab 3 
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Juan Jose Parra y Luisa Maria Vasquez 
+ * @version 11/03/2018
  */
 public class Punto5
 {
+    /**
+     * Método que determina si el grafo tiene o no ciclos
+     * @param g Grafo a revisar
+     * @return Si tiene o no ciclos
+     */
     public static boolean tieneCiclos(Digraph g){
         boolean [] visitados = new boolean [g.size()];
         for(int i = 0; i < visitados.length; i++){
@@ -14,6 +19,14 @@ public class Punto5
         }
         return tieneCiclos(g, visitados, 1);
     }
+    
+    /**
+     * Método recursivo que busca ciclos en un grafo
+     * @param g Grafo en el que se buscan los ciclos
+     * @param visitados Arreglo que almacena los nodos ya visitados
+     * @param origen Nodo inicio
+     * @return Si hay ciclos o no
+     */
     private static boolean tieneCiclos(Digraph g, boolean[] visitados, int origen){
         ArrayList sucesores = g.getSuccessors(origen);
         if(visitados[origen]){
